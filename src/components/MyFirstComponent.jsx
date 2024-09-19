ed413416368dcf3bb01cd4a96629d2a48b532c0e
sebastianalvirap <juanoalvira97@gmail.com>
2024-09-18 20:15:29 -0500

    Update MyFirstComponent and SecondComponent

--- src/components/MyFirstComponent.jsx
+++ src/components/MyFirstComponent.jsx
@@ -1,15 +1,44 @@
-
 export const MyFirstComponent = () => {
-  return (
-    <>
+
+    const name = "Inés María Oliveros";
+  
+    const github_profile = "https://github.com/inesmariao";
+  
+    const student = {
+      name: "Diana ",
+      last_name: "Arévalo",
+      mobile: "300000000",
+      linkedin_profile: "https://www.linkedin.com/in/diana-arevalo-168b0925b"
+    };
+  
+    return (
+      <>
         <div>MyFirstComponent</div>
-        <h1>Temas de React</h1>
-        <ul>
-            <li>Componentes</li>
-            <li>Eventos</li>
-            <li>Estados Hooks</li>
-            <li>Props</li>
-        </ul>
-    </> 
-  )
-}
+        <div className="container bg-info-subtle mt-4">
+          <h1>Temas de React</h1>
+          <ul>
+              <li>Componentes</li>
+              <li>Eventos</li>
+              <li>Estados Hooks</li>
+              <li>Props</li>
+          </ul>
+        </div>
+        <div className="container bg-warning-subtle py-2">
+          <h1>Datos del Docente</h1>
+          <p>Nombre: <strong>{ name }</strong></p>
+          <p>GitHub: { github_profile } </p>
+        </div>
+        <div className="container bg-success-subtle py-2">
+          <h1>Datos del Estudiante</h1>
+          <p>Nombre: <strong>{ student.name }</strong></p>
+          <p>Apellido: { student.last_name } </p>
+          <p>Celular: { student.mobile } </p>
+          <p>LinkedIn: <a href={ student.linkedin_profile }>{ student.linkedin_profile }</a> </p>
+        </div>
+        <div>
+          <h1>Objeto Completo</h1>
+          <pre>{JSON.stringify(student)} </pre>
+        </div>
+      </>
+    )
+  }
